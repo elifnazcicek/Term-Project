a738fae924041da263ef71be4c3b4615be3c2a95
elifnazcicek <elifnazcicek06@gmail.com>
2025-11-18 22:49:35 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,3 @@
+from tables import initialize_tables, add_table, assign_table, release_table, update_server
+from menu import load_menu, save_menu, add_menu_item, update_menu_item, filter_menu
+import json
